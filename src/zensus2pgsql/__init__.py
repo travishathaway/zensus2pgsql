edf373a32c5dcb41a359dbb4400be164734a909a
Travis Hathaway <travis.j.hathaway@gmail.com>
2025-11-22 20:58:05 +0100

    feat: adding version and publishing GHAs

--- src/zensus2pgsql/__init__.py
+++ src/zensus2pgsql/__init__.py
@@ -1,1 +1,12 @@
 """Zensus Collector."""
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    try:
+        from importlib.metadata import version
+
+        __version__ = version("zensus2pgsql")
+        del version
+    except ImportError:
+        __version__ = "0.0.0.unknown"
--- src/zensus2pgsql/cli.py
+++ src/zensus2pgsql/cli.py
@@ -6,7 +6,31 @@ from .commands.create import collect
 from .commands.drop import drop
 from .commands.list import list_datasets
 
+
+def _version_callback(value: bool):
+    if value:
+        from . import __version__
+
+        typer.echo(f"zensus2pgsql version: {__version__}")
+        raise typer.Exit()
+
+
 app = typer.Typer(help="Zensus 2022 Gitterdaten PostgreSQL importer", no_args_is_help=True)
+
+
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the version and exit.",
+        is_eager=True,
+        callback=_version_callback,
+    ),
+):
+    """Zensus 2022 Gitterdaten PostgreSQL importer."""
+
 
 # Add commands directly to the main app
 app.command(name="create")(collect)
